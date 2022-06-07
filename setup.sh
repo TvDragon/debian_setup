2ea112441da5cc9e3ba4fb139d8f89ca5fe10b9f
Tony Vo <tonyvo249@gmail.com>
2022-06-07 15:08:40 +1000

    should be fixed for install

--- setup.sh
+++ setup.sh
@@ -7,24 +7,24 @@
 # apt install sudo
 # Manual (username user)
 # sudo apt update && sudo apt upgrade -y
-# sudo apt install bspwm polybar sxhkd pulseaudio pavucontrol thunar rofi suckless-tools picom xfce4-terminal lxpolkit feh -y # suckless-tools = dmenu, lxpolkit = xfce-polkit (not on debian)
-# sudo apt install lxappearance -y
-# sudo apt install network-manager -y	# Network Manager and NM Gui
-# sudo apt install sddm -y
-# sudo systemctl enable sddm
-# sudo systemctl set-default graphical.target
 # sudo apt install git -y
-# git clone https://github.com/TvDragon/debian_setup_test
+# git clone https://github.com/TvDragon/debian_setup
 
 # Desktop Setup
+sudo apt install bspwm polybar sxhkd pulseaudio pavucontrol thunar rofi suckless-tools picom xfce4-terminal lxpolkit feh -y # suckless-tools = dmenu, lxpolkit = xfce-polkit (not on debian)
+sudo apt install lxappearance -y
+sudo apt install network-manager -y	# Network Manager and NM Gui
 cd
 mkdir .config
 mkdir Desktop Downloads Pictures Videos
 mkdir Pictures/wallpapers
-mv debian_setup_test/wallpapers/* ~/Pictures/wallpapers/
-mv debian_setup_test/dotconfigs/* ~/.config/
-mv debian_setup_test/.xinitrc ~/.
+mv debian_setup/wallpapers/* ~/Pictures/wallpapers/
+mv debian_setup/dotconfigs/* ~/.config/
+mv debian_setup/.xinitrc ~/.
 chmod +x ~/.config/polybar/launch.sh
+sudo apt install sddm -y
+sudo systemctl enable sddm
+sudo systemctl set-default graphical.target
 
 # Install applications
 sudo apt install nano vim -y
