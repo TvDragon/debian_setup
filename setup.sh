ca05c85908ee92c1d7f26944817e154ab568a273
Tony Vo <tonyvo249@gmail.com>
2022-06-07 18:26:51 +1000

    install application to allow for adding to debian repo

--- setup.sh
+++ setup.sh
@@ -53,6 +53,7 @@ curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update && sudo apt install spotify-client -y
 # Install typora
+sudo apt install software-properties-common -y
 wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
 sudo add-apt-repository 'deb https://typora.io/linux ./'
 sudo apt update
