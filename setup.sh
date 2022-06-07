488c17388552617277a52b60154771d72f1b530d
Tony Vo <tonyvo249@gmail.com>
2022-06-07 14:53:45 +1000

    customed

--- setup.sh
+++ setup.sh
@@ -6,14 +6,13 @@
 # apt install sudo
 # Manual (username user)
 # sudo apt update && sudo apt upgrade -y
+# sudo apt install bspwm polybar sxhkd pulseaudio pavucontrol thunar rofi suckless-tools picom xfce4-terminal lxpolkit feh -y # suckless-tools = dmenu, lxpolkit = xfce-polkit (not on debian)
+# sudo apt install lxappearance -y
+# sudo apt install network-manager -y	# Network Manager and NM Gui
 # sudo apt install git -y
 # git clone https://github.com/TvDragon/debian_setup_test
 
 # Desktop Setup
-sudo apt install bspwm polybar sxhkd pulseaudio pavucontrol thunar rofi suckless-tools picom xfce4-terminal lxpolkit feh -y # suckless-tools = dmenu, lxpolkit = xfce-polkit (not on debian)
-sudo apt install lxappearance -y
-sudo apt install network-manager -y	# Network Manager and NM Gui
-sudo apt install nano vim -y
 cd
 mkdir .config
 mkdir Desktop Downloads Pictures Videos
@@ -25,3 +24,6 @@ chmod +x ~/.config/polybar/launch.sh
 sudo apt install sddm -y
 sudo systemctl enable sddm
 sudo systemctl set-default graphical.target
+
+# Install applications
+sudo apt install nano vim -y
