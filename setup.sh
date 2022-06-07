fc0224f905379c2092a0831da8334d6a6e23d975
Tony Vo <tonyvo249@gmail.com>
2022-06-07 15:03:15 +1000

    moved sddm install and enable as manual part

--- setup.sh
+++ setup.sh
@@ -10,6 +10,9 @@
 # sudo apt install bspwm polybar sxhkd pulseaudio pavucontrol thunar rofi suckless-tools picom xfce4-terminal lxpolkit feh -y # suckless-tools = dmenu, lxpolkit = xfce-polkit (not on debian)
 # sudo apt install lxappearance -y
 # sudo apt install network-manager -y	# Network Manager and NM Gui
+# sudo apt install sddm -y
+# sudo systemctl enable sddm
+# sudo systemctl set-default graphical.target
 # sudo apt install git -y
 # git clone https://github.com/TvDragon/debian_setup_test
 
@@ -22,9 +25,6 @@ mv debian_setup_test/wallpapers/* ~/Pictures/wallpapers/
 mv debian_setup_test/dotconfigs/* ~/.config/
 mv debian_setup_test/.xinitrc ~/.
 chmod +x ~/.config/polybar/launch.sh
-sudo apt install sddm -y
-sudo systemctl enable sddm
-sudo systemctl set-default graphical.target
 
 # Install applications
 sudo apt install nano vim -y
