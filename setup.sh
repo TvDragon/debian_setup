7513dbb4fc88d76cb8d36647962d537ccf5f0b77
Tony Vo <tonyvo249@gmail.com>
2022-06-08 16:05:55 +1000

    moved fonts to main and copy into .fonts

--- setup.sh
+++ setup.sh
@@ -14,13 +14,14 @@
 sudo apt install bspwm polybar sxhkd pulseaudio pavucontrol thunar rofi suckless-tools picom xfce4-terminal lxpolkit feh lxappearance -y # suckless-tools = dmenu, lxpolkit = xfce-polkit (not on debian)
 sudo apt install network-manager -y	# Network Manager and NM Gui
 cd
-mkdir .config .icons .themes
+mkdir .config .icons .themes .fonts
 mkdir Desktop Downloads Pictures Videos
 mkdir Pictures/wallpapers
 mv debian_setup/wallpapers/* ~/Pictures/wallpapers/
 mv debian_setup/dotconfigs/* ~/.config/
 mv debian_setup/icons/* ~/.icons/
 mv debian_setup/themes/* ~/.themes/
+mv debian_setup/fonts/* ~/.fonts/
 mv debian_setup/.xinitrc ~/.
 chmod +x ~/.config/bspwm/bspwmrc
 chmod +x ~/.config/polybar/launch.sh
