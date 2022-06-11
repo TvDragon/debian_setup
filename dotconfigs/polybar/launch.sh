26d45bb1907b8bf810ba44519053e21376c6bdfc
Tony Vo <tonyvo249@gmail.com>
2022-06-11 20:57:11 +1000

    updated polybar

--- dotconfigs/polybar/launch.sh
+++ dotconfigs/polybar/launch.sh
@@ -2,4 +2,6 @@
 
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar top 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar xworkspaces 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar xwindow 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar right 2>&1 | tee -a /tmp/polybar2.log & disown
