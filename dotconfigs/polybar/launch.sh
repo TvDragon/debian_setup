eecae074f59794a6d5454a1c9be43f2fd243ba89
Tony Vo <tonyvo249@gmail.com>
2022-06-18 16:27:35 +1000

    changed name of bar

--- dotconfigs/polybar/launch.sh
+++ dotconfigs/polybar/launch.sh
@@ -2,6 +2,4 @@
 
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar xworkspaces 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar xwindow 2>&1 | tee -a /tmp/polybar2.log & disown
-polybar right 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar top 2>&1 | tee -a /tmp/polybar1.log & disown
