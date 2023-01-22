69eb9e391bf6887aa54f8c35a99950681253a721
Tony Vo <tonyvo249@gmail.com>
2023-01-22 17:04:51 +1100

    updated spotify key repo

--- install-software.sh
+++ install-software.sh
@@ -49,7 +49,7 @@ sudo apt install gparted
 # Install mate-calculator
 sudo apt install mate-calc -y
 # Install spotify
-curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
+curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update && sudo apt install spotify-client -y
 Install typora
