66a9977fbba424e77be8eb8d9107f5516188a7d0
Tony Vo <tonyvo249@gmail.com>
2023-07-12 13:22:58 +1000

    pip install

--- install-software.sh
+++ install-software.sh
@@ -59,7 +59,7 @@ sudo nala install ./zoom_amd64.deb -y
 # Install pip3
 sudo nala install python3-pip -y
 # Install pip libraries - flask, pygame, jedi
-pip3 install flask pygame jedi
+sudo nala install python3-flask python3-pygame python3-jedi
 # Install SDL2 Libraries
 sudo nala install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev -y
 # Install neofetch
