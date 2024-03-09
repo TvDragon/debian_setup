57d5bd763ffb18f399a553a5d9251b737e55f5c7
Tony Vo <tonyvo249@gmail.com>
2024-03-09 18:26:05 +1100

    polybar looking nicer

--- dotconfigs-bspwm/polybar/launch.sh
+++ dotconfigs-bspwm/polybar/launch.sh
@@ -2,8 +2,8 @@
 
 # nm-applet
 killall -q nm-applet
-while pgrep -u $UID -x nm-applet >/dev/null; do sleep 1; done
-nm-applet 2>&1 | tee -a /tmp/nm-applet1.log & disown
+# while pgrep -u $UID -x nm-applet >/dev/null; do sleep 1; done
+# nm-applet 2>&1 | tee -a /tmp/nm-applet1.log & disown
 
 # Polybar
 killall -q polybar
